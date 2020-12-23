d34d33ae94a917274ef15e99bf917ee209eca9fb
unknown <lakrainer@edu.aau.at>
2020-12-23 08:25:57 +0100

    implemented route sendMessage --> adds one row in table message

--- whatsAppClone_Service/routes/sendMessage.js
+++ whatsAppClone_Service/routes/sendMessage.js
@@ -1,6 +1,41 @@
+const { rejects } = require('assert');
 const express = require('express');
+const { resolve } = require('path');
 const router = express.Router();
-const fs = require('fs');
+const getDb = require("../db").getDb;
+let db = getDb();
 
+router.post('/' , (req,res) => {
+    let message = req.body;
+    
+    checkValidity(message).then(()=>{
+        db.query("INSERT INTO message VALUES ($1,$2,$3,$4,$5);",[message.messageTo_id,message.messageFrom_id,message.messageText,message.read,message.id]).then(data=>{
+            res.status(200).json({message: "Added row"});
+        }).catch(error => {
+            res.status(404).json({message: "Problems"});
+        });
+    }).catch(error => {
+        res.status(404).json({message: "Row allready exists with this id."});
+    });
+});
+
+let checkValidity = function (message){
+    return new Promise((resolve,reject) => {
+        if(message.id && message.messageTo_id&&message.messageFrom_id&&message.messageText&&message.read){
+            db.query("SELECT * FROM message WHERE id = $1;",[message.id])
+            .then(data=>{
+                if(data.rowCount >0){
+                    reject();
+                }else{
+                    resolve();
+                }
+            }).catch(error=>{
+                console.log("ERROR");
+            });
+        }else{
+            reject();
+        }
+    })
+}
 
 module.exports = router;

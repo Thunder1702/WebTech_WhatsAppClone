dba34355f1d4b20027a7360d3fa2bdb59b1f8090
unknown <lakrainer@edu.aau.at>
2020-12-23 08:38:17 +0100

    implemented route addPhoto --> adds one row to table photo

--- whatsAppClone_Service/routes/uploadPhoto.js
+++ whatsAppClone_Service/routes/uploadPhoto.js
@@ -1,6 +1,41 @@
+const { rejects } = require('assert');
 const express = require('express');
+const { resolve } = require('path');
 const router = express.Router();
-const fs = require('fs');
+const getDb = require("../db").getDb;
+let db = getDb();
 
+router.post('/' , (req,res) => {
+    let photo = req.body;
+    
+    checkValidity(photo).then(()=>{
+        db.query("INSERT INTO photo VALUES ($1,$2,$3,$4);",[photo.id,photo.url,photo.description,photo.photo_uploadedBy_id]).then(data=>{
+            res.status(200).json({message: "Added row"});
+        }).catch(error => {
+            res.status(404).json({message: "Problems"});
+        });
+    }).catch(error => {
+        res.status(404).json({message: "Row allready exists with this id or phone_number."});
+    });
+});
+
+let checkValidity = function (photo){
+    return new Promise((resolve,reject) => {
+        if(photo.id && photo.url&&photo.description&&photo.photo_uploadedBy_id){
+            db.query("SELECT * FROM photo WHERE id = $1;",[photo.id])
+            .then(data=>{
+                if(data.rowCount >0){
+                    reject();
+                }else{
+                    resolve();
+                }
+            }).catch(error=>{
+                console.log("ERROR");
+            });
+        }else{
+            reject();
+        }
+    })
+}
 
 module.exports = router;

0b7aa2a3db18836d204f912b73c3340883859852
tina <kristina.liebhart@gmail.com>
2020-12-29 17:55:02 +0100

    socket.io server setup

---
+++ whatsAppClone_Service/node-realtime-server/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.Server(app);
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+const port = process.env.port || 3000;
+
+io.on ('connection', (socket)=>{
+    console.log('user connected');
+});
+
+server.listen(port, ()=>{
+    console.log('started on port: ${port}');
+});
+
+io.on ('new-message', (message)=>{
+    io.emit(message);
+})

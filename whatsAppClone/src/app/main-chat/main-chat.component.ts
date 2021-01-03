6657ddf38572e79bd957657f6c1017e4a3ba4507
tina <kristina.liebhart@gmail.com>
2021-01-03 23:04:06 +0100

    subscribe

--- whatsAppClone/src/app/main-chat/main-chat.component.ts
+++ whatsAppClone/src/app/main-chat/main-chat.component.ts
@@ -5,26 +5,30 @@ import { Message } from '../message';
 @Component({
   selector: 'app-main-chat',
   templateUrl: './main-chat.component.html',
-  styleUrls: ['./main-chat.component.css']
+  styleUrls: ['./main-chat.component.css'],
+  providers: [ChatService]
 })
 export class MainChatComponent implements OnInit {
 
   newMessage: string;
+  user: string;
   messageList: string[] = [];
 
   constructor(private chatService: ChatService) {
-    chatService.getMessages().subscribe(msg => {
-      this.messageList.push(msg);
-      console.log(msg);
-    })
   }
 
   sendMessage() {
     this.chatService.sendMessage(this.newMessage);
-
     this.messageList.push("me: " + this.newMessage);
     this.newMessage = '';
   }
   ngOnInit() {
+    this.chatService.getMessages().subscribe(msg => {
+      this.messageList.push(msg.msg);
+      console.log(msg);
+      console.log(`from: ${msg.id}`);
+    })
   }
 }
+
+

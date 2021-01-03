c60e4cb50cd2d5d07a0abd0a960ca8a96b3ad583
tina <kristina.liebhart@gmail.com>
2021-01-03 23:02:23 +0100

    connect to one socket

--- whatsAppClone/src/app/chat.service.ts
+++ whatsAppClone/src/app/chat.service.ts
@@ -3,23 +3,57 @@ import { io } from 'socket.io-client';
 import { Observable } from 'rxjs';
 
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class ChatService {
-  private socket;
+
+  static socket;
+  private myID = Math.random();
+
   constructor() {
-    this.socket = io('http://localhost:3000');
+    //setInterval(() => {
+    //  console.log(this.myID);
+    //  console.log(ChatService.socket);
+    //}, 1000);
+  }
+
+  connect(): Promise<void> {
+    ChatService.socket = io('http://localhost:3000');
+    return new Promise((res, rej) => {
+
+      ChatService.socket.on('connect', () => {
+        //console.log(this.myID);
+        console.log("connected!");
+        res();
+        // either with send()
+        //this.socket.send('Hello!');
+
+        // or with emit() and custom event names
+        //this.socket.emit('salutations', 'Hello!', { 'mr': 'john' }, Uint8Array.from([1, 2, 3, 4]));
+      });
+
+    });
   }
 
 
   sendMessage(message: string) {
-    this.socket.emit("sendMessage", message);
+    ChatService.socket.emit("sendMessage", message);
   }
 
-  public getMessages(): Observable<string> {
+  createChat(name: string) {
+    ChatService.socket.emit("createChat", name);
+  }
+
+  getMessages(): Observable<{ id: string, msg: string }> {
     return Observable.create(observer => {
-      this.socket.on('message', (msg: string) => {
+      ChatService.socket.on('message', (msg: string) => {
+        observer.next(msg);
+      });
+    });
+  }
+
+  getNewChats(): Observable<{ id: number, name: string }> {
+    return Observable.create(observer => {
+      ChatService.socket.on('newChat', (msg: string) => {
         observer.next(msg);
       });
     });

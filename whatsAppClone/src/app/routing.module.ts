60b954c634de11e65eda5ce9df9d4be5cd1339b8
unknown <lakrainer@edu.aau.at>
2021-01-06 11:10:52 +0100

    added routes in the routing module

---
+++ whatsAppClone/src/app/routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
+import { MainChatComponent } from './main-chat/main-chat.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'signin' },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signin/register', pathMatch: 'full', redirectTo: 'register' },
+  { path: 'register/signin', pathMatch: 'full', redirectTo: 'signin' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'signin/mainChat', pathMatch: 'full', redirectTo: 'mainChat' },
+  { path: 'mainChat' , component: MainChatComponent},
+  { path: 'mainChat/logout', pathMatch: 'full', redirectTo: 'signin' },
+  { path: 'mainChat/profileSite', pathMatch: 'full', redirectTo: 'profile' },
+  { path: 'profile', component: ProfileComponent},
+  { path: 'profile/backToMainChat' , pathMatch: 'full', redirectTo: 'mainChat'},
+  { path: 'register/mainChat', pathMatch: 'full', redirectTo: 'mainChat' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
+

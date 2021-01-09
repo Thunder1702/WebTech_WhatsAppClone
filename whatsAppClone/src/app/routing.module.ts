24503beb8cbcaa35e73d7d78b3f3095845a6d64f
tina <kristina.liebhart@gmail.com>
2021-01-09 10:34:36 +0100

    routing for contact-list and contact-page

--- whatsAppClone/src/app/routing.module.ts
+++ whatsAppClone/src/app/routing.module.ts
@@ -5,6 +5,8 @@ import { RegisterComponent } from './register/register.component';
 import { MainChatComponent } from './main-chat/main-chat.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'signin' },
@@ -18,7 +20,11 @@ const routes: Routes = [
   { path: 'mainChat/profileSite', pathMatch: 'full', redirectTo: 'profile' },
   { path: 'profile', component: ProfileComponent},
   { path: 'profile/backToMainChat' , pathMatch: 'full', redirectTo: 'mainChat'},
-  { path: 'register/mainChat', pathMatch: 'full', redirectTo: 'mainChat' }
+  { path: 'register/mainChat', pathMatch: 'full', redirectTo: 'mainChat' },
+  { path: 'mainChat/contact-list', pathMatch: 'full', redirectTo: 'contact-list'},
+  { path: 'contact-list' , component: ContactListComponent},
+  { path: 'mainChat/contact-page', pathMatch: 'full', redirectTo: 'contact-page'},
+  { path: 'contact-page', component: ContactPageComponent}
 ];
 
 @NgModule({

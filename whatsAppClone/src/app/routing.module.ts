dd553a8293503c4bd139fb99e37c3732238cdc58
unknown <lakrainer@edu.aau.at>
2021-01-16 10:09:01 +0100

    added routing for contact-list

--- whatsAppClone/src/app/routing.module.ts
+++ whatsAppClone/src/app/routing.module.ts
@@ -25,7 +25,8 @@ const routes: Routes = [
   { path: 'contact-list' , component: ContactListComponent},
   { path: 'mainChat/contact-page', pathMatch: 'full', redirectTo: 'contact-page'},
   { path: 'contact-page', component: ContactPageComponent},
-  { path:'contact-page/backToMainChat', pathMatch: 'full', redirectTo: 'mainChat'}
+  { path:'contact-page/backToMainChat', pathMatch: 'full', redirectTo: 'mainChat'},
+  { path: 'contact-list/mainChat', pathMatch: 'full', redirectTo: 'mainChat'}
 ];
 
 @NgModule({
